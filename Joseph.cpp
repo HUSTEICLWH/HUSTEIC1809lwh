1495d31cb7417fe2c0145880ef5e259019ed8583
HUSTEICLWH <48377846+HUSTEICLWH@users.noreply.github.com>
2019-03-26 22:08:14 +0800

    Update Joseph.cpp

--- Joseph.cpp
+++ Joseph.cpp
@@ -1,1 +1,52 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node{
+  int num;
+  int password;
+  struct node *next;
+ }Josenode,*pnode;
+
+ void Delete(pnode h,int n,int m){
+     int i;
+     pnode p,q;
+     p=h;
+     while(1){
+         for(i=1;i<m;i++){
+             p=p->next;
+         }
+         q=p;
+         while(p->next!=q){
+             p=p->next;
+         }
+         m=q->password;
+         printf("out%d\n",q->num);
+         p->next=q->next;
+         free(q);
+         p=p->next;
+     }
+ }
+ int main() {
+     int n=7,m=20,i;
+     pnode head;
+     head=(Josenode* )malloc(sizeof(pnode));
+     int pw[]={3,1,7,2,4,8,4};
+     head->num=1;
+     head->password=pw[0];
+     pnode p,q;
+     q=head;
+     for(i=1;i<n;i++){
+         p=(Josenode* )malloc(sizeof(pnode));
+         p->num=i+1;
+         p->password=pw[i];
+         q->next=p;
+         p->next=head;
+         q=q->next;
+    }
+    p=head;
+for(i=1;i<=n;i++){ 
+printf("number=%d\tpassword=%d\n",p->num,p->password);
+  p=p->next;
+   }
+  Delete(head,n,m);
+ }
